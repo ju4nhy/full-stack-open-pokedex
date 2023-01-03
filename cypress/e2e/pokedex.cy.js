c404973ba5b210983d7f2a259820dde0e4b854aa
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2023-01-03 15:54:21 +0200

    Added another e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur') // eslint-disable-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.') // eslint-disable-line no-undef
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000') // eslint-disable-line no-undef
+    cy.contains('ivysaur').click() // eslint-disable-line no-undef
+    cy.contains('chlorophyll') // eslint-disable-line no-undef
+  })
 })
