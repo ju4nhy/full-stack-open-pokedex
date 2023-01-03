78943dc83e26bed56bc0544b4c9f18e4e0ed221e
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2023-01-03 15:51:03 +0200

    Fixed linting errors

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,7 @@
 describe('Pokedex', function() {
-    it('front page can be opened', function() {
-      cy.visit('http://localhost:5000') // eslint-disable-line no-undef
-      cy.contains('ivysaur') // eslint-disable-line no-undef
-      cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.') // eslint-disable-line no-undef
-    })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000') // eslint-disable-line no-undef
+    cy.contains('ivysaur') // eslint-disable-line no-undef
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.') // eslint-disable-line no-undef
+  })
 })
