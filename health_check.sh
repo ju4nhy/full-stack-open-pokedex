714a3949eb8b1ddc4b1b9d1e447740c1f3c60e6a
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2023-01-05 14:21:14 +0200

    Testing...

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,13 @@
 
 echo "Hello from shell script"
 
-exit 0 # exit status 0 means that the script "succeeds"
+res=$(curl -s http://localhost:5000/health)
+
+if [ "$res" == "ok" ]; then
+  echo "Succeeded"
+  exit 0 # exit status 0 means that the script "succeeds"
+  fi
+
+echo "Failed"
+
+exit 1 # exit status 1 means that the script "fails"
