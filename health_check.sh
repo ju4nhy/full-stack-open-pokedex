06b528e6a77ed9eeda90d5e5d22bfac39192dc0a
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2023-01-09 13:06:05 +0200

    Added real shell script based healthcheck

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,12 @@
 
 echo "Hello from shell script"
 
-exit 0 # exit status 0 means that the script "succeeds"
+res=$(curl -s http://localhost:5000/health)
+
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0 # exit status 0 means that the script "succeeds"
+  fi
+
+echo "Failed curl to /health"
+exit 1 # exit status 1 means that the script "fails"
